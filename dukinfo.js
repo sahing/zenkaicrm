531724ed08efac3e1c791aa3c8a5af0255c0e4b1
Sahin Ahmed <sahinahmed798@gmail.com>
2021-09-12 17:21:31 +0530

    Updates

--- dukinfo.js
+++ dukinfo.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   console.log("Welcome DukInfo Crm");
   $('#crm_name').html('Sign in to <a href="#" target="_blank" >Zenkai CRM</a> Admin');
-$('#crm_name').parent().append('<h4>for</h4>');
+  $('<h5>for</h5>').insertAfter('#crm_name');
+  //$('#crm_name').parent().append('<h4>for</h4>');
 });
 
